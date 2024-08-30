611641db2c3962e64a8d6624d0375cebe62acde6
Exloz <exloz@hotmail.com>
2024-08-29 23:10:06 -0500

    set nvchad config with Java working

---
+++ init.lua
@@ -0,0 +1,48 @@
+vim.g.base46_cache = vim.fn.stdpath "data" .. "/nvchad/base46/"
+vim.g.mapleader = " "
+
+-- bootstrap lazy and all plugins
+local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
+
+if not vim.uv.fs_stat(lazypath) then
+  local repo = "https://github.com/folke/lazy.nvim.git"
+  vim.fn.system { "git", "clone", "--filter=blob:none", repo, "--branch=stable", lazypath }
+end
+
+vim.opt.termguicolors = true
+vim.opt.rtp:prepend(lazypath)
+
+local lazy_config = require "configs.lazy"
+
+-- load plugins
+require("lazy").setup({
+  {
+    "NvChad/NvChad",
+    lazy = false,
+    branch = "v2.5",
+    import = "nvchad.plugins",
+  },
+
+  { import = "plugins" },
+}, lazy_config)
+
+-- require("cmp").config.formatting = {
+--   format = require("tailwindcss-colorizer-cmp").formatter
+-- }
+
+require("cmp").setup {
+  formatting = {
+    format = require("nvim-highlight-colors").format,
+  },
+}
+
+-- load theme
+dofile(vim.g.base46_cache .. "defaults")
+dofile(vim.g.base46_cache .. "statusline")
+
+require "options"
+require "nvchad.autocmds"
+
+vim.schedule(function()
+  require "mappings"
+end)
---
+++ lua/plugins/init.lua
@@ -0,0 +1,162 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    event = "BufWritePre", -- uncomment for format on save
+    opts = require "configs.conform",
+  },
+
+  -- These are some examples, uncomment them if you want to see them work!
+  -- {
+  --   "neovim/nvim-lspconfig",
+  --   config = function()
+  --     require "configs.lspconfig"
+  --   end,
+  -- },
+  {
+    "mfussenegger/nvim-jdtls",
+    ft = { "java" },
+  },
+  {
+    "nvim-java/nvim-java",
+    lazy = false,
+    dependencies = {
+      "nvim-java/lua-async-await",
+      "nvim-java/nvim-java-core",
+      "nvim-java/nvim-java-test",
+      "nvim-java/nvim-java-dap",
+      "MunifTanjim/nui.nvim",
+      "neovim/nvim-lspconfig",
+      "mfussenegger/nvim-dap",
+      {
+        "williamboman/mason.nvim",
+        opts = {
+          registries = {
+            "github:nvim-java/mason-registry",
+            "github:mason-org/mason-registry",
+          },
+        },
+      },
+    },
+    config = function()
+      require("java").setup {}
+      require("lspconfig").jdtls.setup {
+        on_attach = require("nvchad.configs.lspconfig").on_attach,
+        capabilities = require("nvchad.configs.lspconfig").capabilities,
+        filetypes = { "java" },
+      }
+    end,
+  },
+  -- {
+  --   "WhoIsSethDaniel/mason-tool-installer",
+  --   opts = {
+  --     ensure_installed = {
+  --       "java-debug-adapter",
+  --       "java-test",
+  --     },
+  --   },
+  -- },
+  {
+    "bhundven/vim-trivy",
+    branch = "fixes",
+    lazy = false,
+  },
+
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "vim",
+        "lua",
+        "vimdoc",
+        "html",
+        "css",
+        "tsx",
+        "javascript",
+        "typescript",
+        "java",
+      },
+    },
+  },
+  {
+    "windwp/nvim-ts-autotag",
+    ft = {
+      "javascript",
+      "javascriptreact",
+      "typescript",
+      "typescriptreact",
+    },
+    config = function()
+      require("nvim-ts-autotag").setup()
+    end,
+  },
+  --   {
+  --     "roobert/tailwindcss-colorizer-cmp.nvim",
+  --     lazy = false,
+  --     -- optionally, override the default options:
+  --     config = function()
+  --       require("tailwindcss-colorizer-cmp").setup {
+  --         color_square_width = 2,
+  --       }
+  --     end,
+  --   },
+  {
+    "brenoprata10/nvim-highlight-colors",
+    config = function()
+      require("nvim-highlight-colors").turnOn()
+      require("nvim-highlight-colors").setup {
+        ---Render style
+        ---@usage 'background'|'foreground'|'virtual'
+        render = "foreground",
+
+        ---Set virtual symbol (requires render to be set to 'virtual')
+        virtual_symbol = "■",
+
+        ---Set virtual symbol suffix (defaults to '')
+        virtual_symbol_prefix = "",
+
+        ---Set virtual symbol suffix (defaults to ' ')
+        virtual_symbol_suffix = " ",
+
+        ---Set virtual symbol position()
+        ---@usage 'inline'|'eol'|'eow'
+        ---inline mimics VS Code style
+        ---eol stands for `end of column` - Recommended to set `virtual_symbol_suffix = ''` when used.
+        ---eow stands for `end of word` - Recommended to set `virtual_symbol_prefix = ' ' and virtual_symbol_suffix = ''` when used.
+        virtual_symbol_position = "inline",
+
+        ---Highlight hex colors, e.g. '#FFFFFF'
+        enable_hex = true,
+
+        ---Highlight short hex colors e.g. '#fff'
+        enable_short_hex = true,
+
+        ---Highlight rgb colors, e.g. 'rgb(0 0 0)'
+        enable_rgb = true,
+
+        ---Highlight hsl colors, e.g. 'hsl(150deg 30% 40%)'
+        enable_hsl = true,
+
+        ---Highlight CSS variables, e.g. 'var(--testing-color)'
+        enable_var_usage = true,
+
+        ---Highlight named colors, e.g. 'green'
+        enable_named_colors = true,
+
+        ---Highlight tailwind colors, e.g. 'bg-blue-500'
+        enable_tailwind = false,
+
+        ---Set custom colors
+        ---Label must be properly escaped with '%' to adhere to `string.gmatch`
+        --- :help string.gmatch
+        custom_colors = {
+          { label = "%-%-theme%-primary%-color", color = "#0f1219" },
+          { label = "%-%-theme%-secondary%-color", color = "#5a5d64" },
+        },
+
+        -- Exclude filetypes or buftypes from highlighting e.g. 'exclude_buftypes = {'text'}'
+        exclude_filetypes = {},
+        exclude_buftypes = {},
+      }
+    end,
+  },
+}
